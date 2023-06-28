5db4d8bd2705c21edec17daa7dfa68f664cd2bcb
Liudmyla Tatarenko <liudmyla.tatarenko@gmail.com>
2023-06-28 18:58:52 +0300

    fixed problems with entries

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export const Rank = ( {name, entries}) => {
+export const Rank = ( {name, entries=0}) => {
   return (
     <div>
       <div className="white f3">{`${name}, your current entry count is ${entries}`}</div>
